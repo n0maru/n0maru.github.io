dce092e8dd4d9b3b39df28fbe026084ea3087179
Ryuma Noma <noma11.develop@gmail.com>
2026-07-18 15:00:26 +0900

    feat: public フォルダだけローカルにとってくるようにするための設定

--- init.bat
+++ init.bat
@@ -4,6 +4,13 @@ pushd %~dp0
 
 pip install -r requirements.txt
 
+@REM public フォルダだけローカルにとってくるようにするための設定
+pushd submodules/memo
+git config core.sparsecheckout true
+echo public > ../../.git/modules/submodules/memo/info/sparse-checkout
+popd
+git -C submodules/memo read-tree -mu HEAD
+
 git submodule update --init --recursive
 
 popd

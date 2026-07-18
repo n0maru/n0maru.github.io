b71d02e3ad52096f325f3de7ccbe1f77fc2776c3
Ryuma Noma <noma11.develop@gmail.com>
2026-07-18 17:28:12 +0900

    feat: デプロイ結果の表示

--- deploy.bat
+++ deploy.bat
@@ -5,4 +5,12 @@ pushd %~dp0
 @REM リモートリポジトリの gh-pages ブランチにビルド内容をプッシュする
 mkdocs gh-deploy
 
+if %ERRORLEVEL% equ 0 (
+    echo "デプロイに成功しました。"
+    echo "反映状況は https://github.com/n0maru.github.io/ から確認できます。"
+) else (
+    echo "デプロイに失敗しました。"
+)
+
+
 popd
